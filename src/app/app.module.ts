58368653c8289d38d01c1dd025f81d37c6dc33d9
uiux@okdollar frontend <uiux@okdollar.com>
2025-01-17 14:13:09 +0630

    create: chat-app-clone ui structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatListComponent } from './pages/chat-list/chat-list.component';
+import { MessageComponent } from './pages/message/message.component';
+import { CardComponent } from './components/card/card.component';
+import { SingleLayoutComponent } from './layout/single-layout/single-layout.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatListComponent,
+    MessageComponent,
+    CardComponent,
+    SingleLayoutComponent,
+
   ],
   imports: [
     BrowserModule,
